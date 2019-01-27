e8528da60c3c49b3b0a4123f098d1d43a3a34934
czlhm <563183729@qq.com>
2019-01-27 21:40:57 +0800

    我的页面布局和交互

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- 顶部结构 -->
   <div class="index-container">
+    <!-- 顶部结构 -->
     <div class="search-box">
       <input type="text" placeholder="搜索">
       <icon type="search" size="12"></icon>
@@ -20,6 +20,30 @@
         <p>{{item.name}}</p>
       </div>
     </div>
+    <!-- 楼层区域 -->
+    <div class="floor-container">
+      <div class="floor" v-for="item in floorList">
+        <!-- 顶部 -->
+        <div class="top">
+          <img :src="item.floor_title.image_src" alt>
+          <h3>{{item.floor_title.name}}</h3>
+        </div>
+        <!-- 底部 -->
+        <div class="bottom">
+          <img v-for="(it,i)  in item.product_list" :src="it.image_src" alt>
+        </div>
+      </div>
+    </div>
+    <!-- 底部结构 -->
+    <div class="bottom-line">
+      <i class="iconfont icon-xiao"></i>
+      我是有底线的
+    </div>
+    <!-- 返回顶部 -->
+    <div class="back-top" @click="tTop" v-show="isShow">
+      <i class="iconfont icon-jiantoushang"></i>
+      顶部
+    </div>
   </div>
 </template>
 
@@ -32,64 +56,108 @@ export default {
       //轮播图数组
       swiperList: [],
       //分类的数据
-      categoryList: []
+      categoryList: [],
+      //楼层的数据
+      floorList: [],
+      //是否显示返回到顶部
+      isShow: false
     };
+  },
+  methods: {
+    //返回顶部
+    tTop() {
+      wx.pageScrollTo({
+        scrollTop: 0, //滚动到页面的目标位置（单位px）,
+        duration: 300 //滚动动画的时长，默认300ms，单位 ms,
+      });
+    }
+  },
+  //滚动事件
+  onPageScroll(event) {
+    if (event.scrollTop > 170) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
   },
   //初始化的时候 获取数据
   async created() {
     //   轮播图数据
-    let swiperRes = await hxios.get({
+    // let swiperRes = await hxios.get({
+    //   url: "api/public/v1/home/swiperdata"
+    // });
+    // this.swiperList = swiperRes.data.message;
+    // //分类接口数据
+    // let cateGoryRes = await hxios.get({
+    //   url: "api/public/v1/home/catitems"
+    // });
+    // this.categoryList = cateGoryRes.data.message;
+    // //获取楼层数据
+    // let floorRes = await hxios.get({
+    //   url: "api/public/v1/home/floordata"
+    // });
+    // this.floorList = floorRes.data.message;
+
+    //一次获取三个
+    let p1 = hxios.get({
       url: "api/public/v1/home/swiperdata"
     });
-    this.swiperList = swiperRes.data.message;
-    //分类接口数据
-    let cateGoryRes = await hxios.get({
+    let p2 = hxios.get({
       url: "api/public/v1/home/catitems"
     });
-    this.categoryList = cateGoryRes.data.message;
+    let p3 = hxios.get({
+      url: "api/public/v1/home/floordata"
+    });
+
+    // 一次性发送所有请求
+    let totalRes = await Promise.all([p1, p2, p3]);
+    this.swiperList = totalRes[0].data.message;
+    this.categoryList = totalRes[1].data.message;
+    this.floorList = totalRes[2].data.message;
   }
 };
 </script>
 
 <style lang="scss">
 $uRed: #ff2d4a;
-// 顶部的样式
+//
 .index-container {
   padding-top: 100rpx;
 }
+// 搜索框的样式
 .search-box {
-  background-color: $uRed;
+  background: $uRed;
   padding: 20rpx 16rpx;
   box-sizing: border-box;
   position: fixed;
-  width: 100%;
   left: 0;
   top: 0;
+  width: 100%;
+  z-index: 998;
   input {
-    display: flex;
+    display: block;
     width: 100%;
     box-sizing: border-box;
     padding-left: 376rpx;
     background-color: white;
-    height: 60rpx;
-    border-radius: 8rpx;
     font-size: 24rpx;
-    margin-right: 20rpx;
+    height: 60rpx;
+    border-radius: 10rpx;
   }
   icon {
     position: absolute;
-    //父盒子
+    // 父盒子
     left: 50%;
     top: 50%;
-    //自己
+    // 自己
     transform: translate(-50%, -50%);
   }
 }
-//轮播图样式
+// 轮播图的样式
 .swiper-container {
   swiper {
     height: 340rpx;
-    .swiper-item {
+    swiper-item {
       height: 100%;
     }
   }
@@ -99,7 +167,7 @@ $uRed: #ff2d4a;
     height: 100%;
   }
 }
-// 分类的样式
+// 分类按钮区域
 .category-container {
   display: flex;
   padding-top: 24rpx;
@@ -119,5 +187,78 @@ $uRed: #ff2d4a;
       font-size: 24rpx;
     }
   }
+}
+
+// 楼层数据
+.floor-container {
+  .floor {
+    .top {
+      padding-top: 30rpx;
+      padding-bottom: 30rpx;
+      padding-left: 15rpx;
+      position: relative;
+      height: 90rpx;
+      box-sizing: border-box;
+      h3 {
+        color: #ff7b94;
+        position: absolute;
+        left: 30rpx;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      img {
+        position: absolute;
+        height: 90rpx;
+        left: 0;
+        top: 0;
+        display: block;
+        width: 100%;
+      }
+    }
+    .bottom {
+      padding: 20rpx 0 0 16rpx;
+      overflow: hidden;
+      img {
+        display: block;
+        width: 33.333%;
+        float: left;
+        width: 230rpx;
+        height: 190rpx;
+        margin-right: 10rpx;
+        &:first-child {
+          width: 230rpx;
+          height: 390rpx;
+        }
+        &:nth-child(2) {
+          margin-bottom: 10rpx;
+        }
+        &:nth-child(3) {
+          margin-bottom: 10rpx;
+        }
+      }
+    }
+  }
+}
+
+//底线的样式
+.bottom-line {
+  display: flex;
+  justify-content: center;
+  color: #999;
+  font-size: 24rpx;
+  margin-top: 20rpx;
+}
+
+//返回顶部
+.back-top {
+  position: fixed;
+  bottom: 15rpx;
+  right: 15rpx;
+  text-align: center;
+  width: 90rpx;
+  height: 90rpx;
+  border-radius: 50%;
+  background-color: antiquewhite;
+  font-size: 24rpx;
 }
 </style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,11 +1,230 @@
 <template>
-<div></div>
+  <div class="mime-container">
+    <!-- 头部 -->
+    <div class="header">
+      <div class="icon-box">
+        <i class="iconfont icon-shezhi"></i>
+        <button open-type="getUserInfo" @getuserinfo="getUserInfo">
+          <img :src="icon" alt>
+        </button>
+        <i class="iconfont icon-xiaoxi"></i>
+      </div>
+      <p>{{info}}</p>
+    </div>
+    <!-- 中间的结构 -->
+    <div class="body-box">
+      <!-- 历史 -->
+      <div class="history-box">
+        <div class="item">
+          <p>0</p>
+          <p>收藏店铺</p>
+        </div>
+        <div class="item">
+          <p>0</p>
+          <p>收藏商品</p>
+        </div>
+        <div class="item">
+          <p>0</p>
+          <p>关注的商品</p>
+        </div>
+        <div class="item">
+          <p>0</p>
+          <p>我的足迹</p>
+        </div>
+      </div>
+      <!-- 订单 -->
+      <div class="order-box">
+        <div class="title">我的订单</div>
+        <div class="bottom">
+          <div class="item">
+            <i class="iconfont icon-daifukuan"></i>
+            待付款
+          </div>
+          <div class="item">
+            <i class="iconfont icon-daishouhuo"></i>
+            待收货
+          </div>
+          <div class="item">
+            <i class="iconfont icon-tuikuan"></i>
+            退款/退货
+          </div>
+          <div class="item">
+            <i class="iconfont icon-dingdan"></i>
+            全部订单
+          </div>
+        </div>
+      </div>
+      <!-- 选线区域 -->
+      <div class="option-box">
+        <!-- 收货地址 -->
+        <div class="option" @click="getAddress">
+          <div class="left">收货地址管理</div>
+          <div class="right">
+            <i class="iconfont icon-jiantouyou"></i>
+          </div>
+        </div>
+      </div>
+      <div class="option-box">
+        <!-- 客服和意见 -->
+        <div class="option" @click="callKF">
+          <div class="left">收货地址管理</div>
+          <div class="right">400-618-400</div>
+        </div>
+        <div class="option">
+          <div class="left">意见反馈</div>
+          <div class="right">
+            <i class="iconfont icon-jiantouyou"></i>
+          </div>
+        </div>
+        <div class="option">
+          <div class="left">关于我们</div>
+          <div class="right">
+            <i class="iconfont icon-jiantouyou"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      icon: "/static/icon.png",
+      info: "登录/注册"
+    };
+  },
+  //方法
+  methods: {
+    getUserInfo(event) {
+      this.icon = event.mp.detail.userInfo.avatarUrl;
+      this.info = event.mp.detail.userInfo.nickName;
+    },
+    // 地址获取
+    getAddress() {
+      wx.chooseAddress({
+        success(res) {
+          console.log(res.userName);
+          console.log(res.postalCode);
+          console.log(res.provinceName);
+          console.log(res.cityName);
+          console.log(res.countyName);
+          console.log(res.detailInfo);
+          console.log(res.nationalCode);
+          console.log(res.telNumber);
+        }
+      });
+    },
+    //打电话给客服
+    callKF() {
+      wx.makePhoneCall({
+        phoneNumber: "13710037593" // 仅为示例，并非真实的电话号码
+      });
+    }
+  }
+};
 </script>
 
-<style scoped>
-
+<style lang="scss">
+$uRed: #ff2d4a;
+page {
+  background-color: #0094ff;
+}
+// 头部的样式
+.header {
+  height: 430rpx;
+  background-color: $uRed;
+  padding-top: 120rpx;
+  box-sizing: border-box;
+  .icon-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .iconfont {
+      color: white;
+    }
+    button {
+      margin: 0 65rpx;
+      width: 130rpx;
+      border-radius: 50%;
+      height: 130rpx;
+      overflow: hidden;
+      border: 5rpx solid white;
+      padding: 0;
+      img {
+        width: 130rpx;
+        height: 130rpx;
+      }
+    }
+  }
+  p {
+    text-align: center;
+    margin-top: 20rpx;
+    color: white;
+    font-size: 28rpx;
+  }
+}
+// 中间的样式
+.body-box {
+  transform: translateY(-26rpx);
+  padding: 0 20rpx;
+  > div {
+    background-color: white;
+  }
+  //历史样式
+  .history-box {
+    height: 120rpx;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #aaa;
+    font-size: 22rpx;
+    .item {
+      flex: 1;
+    }
+  }
+  //我的订单样式
+  .order-box {
+    margin-top: 20rpx;
+    .title {
+      height: 90rpx;
+      border-bottom: 1rpx solid #aaa;
+      line-height: 90rpx;
+      font-size: 30rpx;
+      padding-left: 30rpx;
+    }
+    .bottom {
+      display: flex;
+      align-items: center;
+      text-align: center;
+      height: 130rpx;
+      .item {
+        flex: 1;
+        font-size: 24rpx;
+        i {
+          color: $uRed;
+          font-size: 60rpx;
+          margin-bottom: 10rpx;
+        }
+      }
+    }
+  }
+  //选线区域
+  .option-box {
+    margin-top: 20rpx;
+    padding-left: 30rpx;
+    .option {
+      height: 84rpx;
+      font-size: 28rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1rpx solid #aaa;
+      &:last-child {
+        border-bottom: none;
+      }
+    }
+  }
+}
 </style>
